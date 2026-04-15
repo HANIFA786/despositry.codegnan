aae1b311a42c1182f3f8cb9fccf1feb184af62e1
HANIFA786 <141350240+HANIFA786@users.noreply.github.com>
2026-04-15 03:02:07 -0700

    Add files via upload
    My first day in codegnan

---
+++ day1.py
@@ -0,0 +1,17 @@
+'''
+python 3.13.11 (tags/v3.13.11:6278944, Dec  5 2025, 16:26:58) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+what is python?
+-->Python is a high-level language which means no need to worry about low-level 
+management like memory allocation.
+-->Python never save anything perminently, it will only save at a time
+-->Python supports OOPs it will have a feature where only the user can give the input 
+where we will not define the value 
+
+
+'''
+a=10
+b=7
+print(a+b)
+print(10+9)
+a=int(input())
