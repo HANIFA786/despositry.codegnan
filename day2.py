fa2a77e31febc4f0c2aff62f0ce8b25b66e2fa67
HANIFA786 <141350240+HANIFA786@users.noreply.github.com>
2026-04-15 22:05:37 -0700

    Add files via upload
    Day 2 in Codegnan 
    Learned about these new things and worked on them

---
+++ day2.py
@@ -0,0 +1,74 @@
+Variable
+
+----------------------------------------
+To define a variable, we have 2 rules
+1. Good way to define a varibale (No error)
+2. Bad way to define a varibale ( will get errors)
+
+Note
+
+--------------
+--> We are going to use meaningful words or name for defining variables
+
+
+
+Keywords
+------------------
+
+--> This keywords are not gonna use as a varibale
+--> if, else, for where we wont be use that in the programs
+
+''' 
+'''
+
+a,b,c=45,64,78
+print=(a)
+print=(b)
+print=(c)
+'''
+'''
+
+Tokens
+--------------
+--> Nothing but a small program in python to complete the task
+
+'''
+'''
+
+a, b, c = 23, 56, 78
+
+print(a+b)
+print(b-c)
+print(a+b+c)
+'''
+'''
+Comments
+---------
+--> These are 2 type comments
+1.Single line comments
+--> This is used to explain about the particular line in a code, for this we can use # symbol
+2.Multi-line
+--> To comment more than single line we can use multi-line comment
+"""  """, ''' '''
+where we can use double coats, or single coats
+
+Note
+That wont be considered anywhere in the program
+
+'''
+'''
+a, b, c = 37, 56, 78 #here is the line, i have assign 3 variable and value
+'''
+'''
+Boolean Type
+--> This is used to find out that whether the statement is true or false '''
+
+a= 90
+b= 9
+print(a==b)
+
+num=7
+if num%2==0:
+    print(f"{num} is even number")
+else:
+    print(f"{num} is odd number")
