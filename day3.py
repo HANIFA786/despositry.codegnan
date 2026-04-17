d3cb081ed366e7fd9e6f4c677bace2fdfb054997
HANIFA786 <141350240+HANIFA786@users.noreply.github.com>
2026-04-16 22:29:59 -0700

    Add files via upload
    Today learnt about the operators
    How operators are used and how many operators are there 
    And also learnt what the main operators that we will be using are

---
+++ day3.py
@@ -0,0 +1,226 @@
+'''
+
+Operators
+1.Arithmetic operator
+2.Assignment operator
+3.comparision operators
+4.Logical operator
+5.Identity
+6.Membership
+7.Bitwise
+
+
+1.Arithmetic Operator:   +, -, %, *, **(power)
+
+Num= 90
+Num_2= 89
+Num_3= 78
+print(Num + Num_2+Num_3)
+
+Num= 6
+num_2= 7
+NUm_3= 9
+print(Num-num_2-NUm_3)
+
+NuM=56
+num=8
+print(NuM%num)
+
+
+digit_=9
+digit_2=3
+print(digit_**digit_2)
+
+
+digi_=7.86
+digi_2=6.56
+print(digi_//digi_2)'''
+
+'''
+Assignment Operator
+
+=, +=, -=, *=,**=,%=
+  = To assign the datatype to the variable
+
+number =9
+number =number+4
+print(number)
+
+number =9
+number =number-4
+print(number)
+
+number =9
+number =number*4
+print(number)
+
+number =9
+number **= 4
+print(number)
+
+number =9.78
+number //= 4
+print(number)
+'''
+
+
+'''
+Comparison Operator
+==, !=, >, <, >=, <=
+
+
+a=8
+b=8
+print(a==b)
+
+o/p= true
+   
+a=8
+b=9
+print(a!=b)
+
+o/p= true
+
+a=8
+b=9
+print(a<=b)
+
+O/p= true
+
+a=8
+b=9
+print(a>=b)
+
+o/p= false
+'''
+
+
+'''
+Logical Operator
+
+and--> when the 2 condition should be true
+
+
+h= 8
+q= 78
+print(h > 10 and q > 10)
+
+
+h= 8
+q= 78
+print(h < 10 and q > 10)
+
+
+h= 8
+q= 78
+print(h < 10 or q > 10)
+
+h= 8
+q= 78
+print(not (h > 10 and q < 10))
+
+Is--> This operatoris used to check the object
+
+
+
+h=7
+q=7
+print(h is q)
+list_= [1,2] 
+list_2=[1,2]
+list_3=[1,2]
+print(id(list_ is list_3))
+
+
+list_= [1,2] 
+list_2=[1,2]
+print(id(list_ is not list_2))
+print(list_==list_2)
+
+
+is not-->  This operator is used to check the object not same
+
+
+Membership operator
+
+In operator =Is used to check, if it present in it or not.
+
+list_ = [1,2]
+print(2 in list_)
+
+
+&-->Bitwise AND
+
+
+5-->0101
+3-->0011
+
+1-->0001
+'''
+
+print(5&3)
+'''
+^-->Bitwise XOR
+6-->0110
+
+print(5^3)
+
+Bitwise OR|
+
+7--> 0111'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
