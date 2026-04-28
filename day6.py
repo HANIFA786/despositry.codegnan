5ea63b39914c068467a4fb608bb35fe4c8490c91
HANIFA786 <141350240+HANIFA786@users.noreply.github.com>
2026-04-28 02:38:28 -0700

    Explored tuples in Python
    Learned that a tuple is a collection of different data types

---
+++ day6.py
@@ -0,0 +1,126 @@
+''' sorted'''
+
+
+
+
+
+
+
+
+
+
+'''
+Tuple
+--------------------------------------------------
+it is collection of f=different data types that are represented by () and the items in the tuples will be separated by , commas..
+
+so =  (1,2,3,4)
+any = (5,6,7)
+print(so + any)
+
+
+
+Dicitionary-----DICT
+-------------------------------------------------------------------------------------------------------------
+Dict is a collection of key :  value pair, where keys are immutable such as (string, int and tuple) and values
+and also duplicate keys are not allowed
+and which is representd by {}
+
+
+my = {"Name" : "Hanifa",
+      "age": 56,
+      "Edu" :"MCA"}
+
+print(type(my))
+
+
+Methods
+----------------------------------------
+keys()
+
+this method is used to access only keys in the dicitionary
+syntax: dict.keys()
+
+my = {"Name" : "Hanifa",
+      "age": 56,
+      "Edu" :"MCA"}
+
+print(my.keys())
+
+-----------------------------------------------------------
+values()
+
+This is used to access only the values in the dictionary
+syntax-->dict.values
+
+my = {"Name" : "Hanifa",
+      "age": 56,
+      "Edu" :"MCA"}
+
+print(my.values())
+------------------------------------------------------------------------------
+items()
+
+this method is used to access key : value pair in the dictionary
+syntax-->  dict.items
+
+my = {"Name" : "Hanifa",
+      "age": 56,
+      "Edu" :"MCA"}
+print ("Hanifa" in my ['Name'])
+
+my is the dicitionary
+name is the key
+-------------------------------------------------------------------
+
+Clear()
+------------------------------------------------
+this clear() method is used to delete all the items in the dict
+
+syntax-->  dict.clear
+
+
+
+update
+----------------------------------------------------------------
+this method is used to add new item(key : value) into the dictionary
+syntax-------dict.update({Key : Value})
+
+
+my = {"Name" : "Hanifa",
+      "age": 56,
+      "Edu" :"MCA"}
+my.update({"Role" : "Python Developer"})
+print(my)'''                                                                                                                                                                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
